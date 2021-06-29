0fe2c87a863aa03cba3273cdbcc41a0d4b74def7
Ammie Wang <ammie.wang8@gmail.com>
2021-06-29 03:03:02 -0400

    mobile sidebar

--- site.js
+++ site.js
@@ -1,18 +1,9 @@
-function addGrid() {
-  var gridCont = document.getElementById('gridCont')
-  var cell = document.getElementById('cell')
-  var newBox
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 20; j++) {
-      newBox = document.createElement('div')
-      if (i == 0 && j == 0) {
-        newBox.className = 'firstRow firstInRow'
-      } else if (i == 0) {
-        newBox.className = 'firstRow'
-      } else if (j == 0) {
-        newBox.className = 'firstInRow'
-      }
-      gridCont.appendChild(newBox)
-    }
-  }
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
