a96631a43acd70cae165ab6e1779f0070b9a8f26
Ammie Wang <ammie.wang@yahoo.com>
2021-12-26 21:30:40 -0800

    make cards clickable

--- site.js
+++ site.js
@@ -7,3 +7,8 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+function flipCard(id) {
+  var card = document.getElementById(id).style.transform;
+  document.getElementById(id).style.transform = (card == "rotateY(180deg)" ? "" : "rotateY(180deg)");
+}
